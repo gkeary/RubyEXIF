a88d34c476cc389c7a352cf743d374b38ec188b4
gkeary <gregg@gkeary.com>
2010-01-14 06:50:58 -0500

    started cleanup effort

--- my_test.rb
+++ my_test.rb
@@ -6,13 +6,13 @@ class MyTest < Test::Unit::TestCase
   # Called before every test method runs. Can be used
   # to set up fixture information.
   def setup
-    @@d = Exif.new
+    @@d = PhotoCollection.new
   end
 
   def test_jpgfiles
   #MyTest::setup()
-      assert_equal(13, @@d.get_size())
-      assert_operator(@@d.get_size(), :>, 0)
+      assert_equal(13, @@d.get_jpgfiles_size())
+      assert_operator(@@d.get_jpgfiles_size(), :>, 0)
   end
 
   def test_get_first_width
@@ -21,6 +21,22 @@ class MyTest < Test::Unit::TestCase
   # Called after every test method runs. Can be used to tear
   # down fixture information.
 
+  def test_photo_load
+    z = @@d.load_photo     
+    assert_operator(z.size, :>, 0)
+  end
+
+#  def test_dupe_load
+#    z = @@d.load_photo
+#    hash = Photo::get_hash
+#    i = 0
+#    z = hash.values
+#    max = hash.values { |x|
+#      i = i < x ? x : i++
+#      return i
+#      }
+#    assert_operator( max, :>, 2 )
+#  end
   def teardown
     # Do nothing
   end
